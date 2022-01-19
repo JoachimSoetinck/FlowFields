8669e73559d454967a15b48d4913c12ba9d4784d
LAPTOP-C3BB0KG3\joach <joachim.soetinck99@gmail.com>
2022-01-19 14:17:29 +0100

    Research topic unity; Working flowfield with agents

---
+++ Flowfields/Assets/Scripts/AgentSpawner.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentSpawner : MonoBehaviour
+{
+	public GridController gridController;
+	public GameObject agent;
+
+	[SerializeField]
+	private int AmountAgents;
+
+	[SerializeField]
+	private float moveSpeed;
+
+	private List<GameObject> agents;
+
+	private void Awake()
+	{
+		agents = new List<GameObject>();
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			SpawnAgents();
+		}
+
+	}
+
+	private void FixedUpdate()
+	{
+		if (gridController.grid == null) {
+			Debug.Log("No Grid available");
+			return; 
+		}
+		foreach (GameObject agent in agents)
+		{
+			//getting the cell where the agent is standing
+			Cell cell = gridController.grid.GetCellFromWorldPos(agent.transform.position);
+			
+			Vector3 moveDirection = new Vector3(cell.bestDirection.x, 0, cell.bestDirection.y);
+			
+			Rigidbody unitRB = agent.GetComponent<Rigidbody>();
+			unitRB.velocity = moveDirection * moveSpeed;
+		}
+	}
+
+	private void SpawnAgents()
+	{
+		//Spawning the Agents in the grid
+		Vector2Int gridSize = gridController.gridSize;
+		float nodeRadius = gridController.cellRadius;
+
+		//getting the max coordinates a unit can spawn
+		Vector2 maxSpawnPos = new Vector2(gridSize.x * nodeRadius * 2 + nodeRadius, gridSize.y * nodeRadius * 2 + nodeRadius);
+
+
+		int colMask = LayerMask.GetMask("Wall", "Agent");
+		Vector3 newPos;
+		for (int i = 0; i < AmountAgents; i++)
+		{
+			GameObject newUnit = Instantiate(agent);
+			newUnit.transform.position = transform.position;
+			agents.Add(newUnit);
+			do
+			{
+				newPos = new Vector3(Random.Range(0, maxSpawnPos.x), 0, Random.Range(0, maxSpawnPos.y));
+				newUnit.transform.position = newPos;
+			}
+			while (Physics.OverlapSphere(newPos, 0.25f, colMask).Length > 0); //checking collision so that units dont spawn in a wal
+		}
+	}
+
+	private void DestroyUnits()
+	{
+		foreach (GameObject agent in agents)
+		{
+			Destroy(agent);
+		}
+		agents.Clear();
+	}
+}
---
+++ Flowfields/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GridController gridController;
+    public GameObject walPrefab;
+    public GameObject GrassPrefab;
+    bool CanSpawnGrass = false;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            CanSpawnGrass = !CanSpawnGrass;
+        }
+
+            if (Input.GetMouseButtonDown(1))
+        {
+            if (CanSpawnGrass == false)
+            {
+                Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
+                Vector3 worldPosMouse = Camera.main.ScreenToWorldPoint(mousePos);
+                SpawnWall(worldPosMouse);
+                gridController.grid.GenerateIntegrationField(gridController.Destination);
+            }
+            else
+            {
+                Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
+                Vector3 worldPosMouse = Camera.main.ScreenToWorldPoint(mousePos);
+                SpawnGrass(worldPosMouse);
+                gridController.grid.GenerateIntegrationField(gridController.Destination);
+            }
+        }
+    }
+
+
+    private void SpawnWall(Vector3 wordlPos)
+    {
+        Vector2Int gridSize = gridController.gridSize;
+        float nodeRadius = gridController.cellRadius;
+        Vector2 maxSpawnPos = new Vector2(gridSize.x * nodeRadius * 2 + nodeRadius, gridSize.y * nodeRadius * 2 + nodeRadius);
+      
+        GameObject newUnit = Instantiate(walPrefab, wordlPos, Quaternion.identity);
+        newUnit.transform.parent = transform;
+
+  
+    }
+
+    private void SpawnGrass(Vector3 wordlPos)
+    {
+        Vector2Int gridSize = gridController.gridSize;
+        float nodeRadius = gridController.cellRadius;
+
+        Vector2 maxSpawnPos = new Vector2(gridSize.x * nodeRadius * 2 + nodeRadius, gridSize.y * nodeRadius * 2 + nodeRadius);
+        GameObject newUnit = Instantiate(GrassPrefab, wordlPos, Quaternion.identity);
+        newUnit.transform.parent = transform;
+    }
+}
